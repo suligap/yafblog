348eed91169207ce5680f455f6cad65f7e03eead
Przemysław Suliga <suligap@gmail.com>
2013-08-27 20:32:41 +0200

    Added heroku enablers

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn yafblog:app
--- yafblog/__init__.py
+++ yafblog/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
@@ -5,10 +7,17 @@ from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/yafblog-db'
+try:
+    # retarded env detection
+    _db_uri = os.environ['DATABASE_URL']
+    app.secret_key = os.environ['SECRET_KEY']
+    app.debug = False
+except KeyError:
+    _db_uri = 'postgresql://localhost/yafblog-db'
+    app.debug = True
+    app.secret_key = 'secret-key-debug'
 
-app.debug = True  # TODO
-app.secret_key = 'secret-key-debug'  # TODO
+app.config['SQLALCHEMY_DATABASE_URI'] = _db_uri
 
 
 db = SQLAlchemy(app)
